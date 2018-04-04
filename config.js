17420c6330ae0f66c223feb463dcd82866400f1b
penglin <plin@qq.com>
2018-04-04 12:48:45 +0800

    x

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports =  [
+	{
+		input: 'src/button/index.js',
+		output: 'dist/button/index.js',
+		cssinput: 'src/button/style.less',
+		cssoutput: 'dist/button/'
+	},
+	{
+		input: 'src/pop/index.js',
+		output: 'dist/pop/index.js',
+		cssinput: 'src/pop/style.less',
+		cssoutput: 'dist/pop/'
+	},
+	{
+		input: 'src/Progress/index.js',
+		output: 'dist/progress/index.js',
+		cssinput: 'src/Progress/style.less',
+		cssoutput: 'dist/progress/'
+	},
+	{
+		input: 'src/main.js',
+		output: 'dist/index.js',
+		cssinput: 'src/style/style.less',
+		cssoutput: 'dist/'
+	}
+]
