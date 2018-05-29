b959952f974e6148ea9c9fa50082404299dc0f4d
penglin <plin@qq.com>
2018-05-29 15:07:30 +0800

    x

--- src/Progress/index.js
+++ src/Progress/index.js
@@ -23,7 +23,6 @@ let progress = {
 			this.intval = setInterval(() => {
 				this.count++
 				this.opacity = this.opacity > 1 ? 1 : this.opacity
-				console.log(this.count,1)
 				if (this.count >= 95) {
 					clearInterval(this.intval)
 				}
@@ -39,7 +38,7 @@ let progress = {
 					this.opacity = 0
 				}, 100)
 			} else {
-				// this.finish()
+				this.finish()
 			}
 
 		}
